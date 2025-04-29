8b9a135b8de1d4d80a992441f15a4546266e77a7
Zach-Stuart <zachstuart@mmc.school.nz>
2025-04-30 10:08:49 +1200

    function to make single-line decorated headings

---
+++ make_statement.py
@@ -0,0 +1,9 @@
+# functions
+def make_statement(statement, decoration):
+    """"emphasises headings by adding decoration on the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# main routine
+make_statement("gntghngdgb", decoration="*")
